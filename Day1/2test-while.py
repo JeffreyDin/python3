90c40baf88b265d17dc4f5413e45d37f32475973
gunxiaoshi <gunxiaoshi@gmail.com>
2018-11-20 18:45:22 +0800

    This is just my learning.
    Signed-off-by: gunxiaoshi <gunxiaoshi@gmail.com>

--- Day1/2test-while.py
+++ Day1/2test-while.py
@@ -8,9 +8,19 @@
 
 # 斐波纳契数列,编程第一步。
 # b=1,a:1 b:1.  b=1,a:1 b:2.   b=2,a:2 b:3.   b=3,a:3 b:5.   b=5,a:5 b:8.  b=8,a:8 b:13.  b=13
-a, b = 0, 1
-while b < 10:
-    print(b)
-    a, b = b, a+b
+
+# a, b = 0, 1
+# while b < 10:
+#     print(b)
+#     a, b = b, a+b
 
 
+# 计算1到100的总和
+
+n = 100
+sum = 0
+counter = 1
+while counter <= n:
+    sum = sum + counter
+    counter += 1
+print("1到%d之和为：%d" % (n, sum))
