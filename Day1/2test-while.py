aa1ea3e8084d13320cf78dc5b8f773cd4eaae094
gunxiaoshi <gunxiaoshi@gmail.com>
2018-11-20 20:15:43 +0800

    This is just my learning.
    Signed-off-by: gunxiaoshi <gunxiaoshi@gmail.com>

--- Day1/2test-while.py
+++ Day1/2test-while.py
@@ -15,7 +15,7 @@
 #     a, b = b, a+b
 
 
-# 计算1到100的总和
+# while循环之计算1到100的总和
 n = 100
 summer = 0
 counter = 1
@@ -24,4 +24,17 @@ while counter <= n:
     counter += 1
 print("1到%d之和为：%d" % (n, summer))
 
-adsf
+# while无限循环
+# var = 1
+# while var == 1:  # 表达式永远为 true
+#     num = int(input("输入一个数字  :"))
+#     print("你输入的数字是: ", num)
+# print("Good bye!")
+
+# while 循环使用 else 语句
+count = 0
+while count < 5:
+    print(count, "小于5")
+    count = count + 1
+else:
+    print(count, "大于或等于5 ")
