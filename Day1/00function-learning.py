83abf86bdaa75556ab084dde013255c4432626d5
gunxiaoshi <gunxiaoshi@gmail.com>
2018-11-22 11:56:27 +0800

    This is just my learning.
    Signed-off-by: gunxiaoshi <gunxiaoshi@gmail.com>

---
+++ Day1/00function-learning.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Time    : 2018/11/22 10:47
+# @Author  : Jianfeng Ding
+# @Site    : 
+# @File    : 00test.py
+# @Software: PyCharm
+
+# rang()函数，指定区间的值，遍历不同的步长；创建列表
+# for i in range(5, 10):
+# for i in range(0, 10, 3):
+#     print(i)
+#
+# list(range(5))
+
+
+# len()函数+range函数遍历序列索引
+a =  ['Google', 'Baidu', 'Runoob', 'Taobao', 'QQ']
+for i in range(len(a))
