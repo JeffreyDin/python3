7dae8c1473ea83813d65f515838c31d207ff887e
gunxiaohsi <gunxiaoshi@gmail.com>
2019-01-21 15:34:34 +0800

    This is just my again learning。

--- Day4Library-file/3.0.0os_test.py
+++ Day4Library-file/3.0.0os_test.py
@@ -6,20 +6,31 @@
 # @File    : 3.0.0os_test.py
 # @Software: PyCharm
 
+
+# os.path — Common pathname manipulations
 import os
 
+print(os.path.abspath('.'))
+# 当前的绝对路径
 print(os.path.abspath('..'))
+# 当前的上级目录
 print(os.path.exists('F:\System_ISO'))
+# 判断文件(文件夹和文件)是否存在
 print(os.path.isfile('users'))
 print(os.path.isfile('2.0.0math.py'))
 print(os.path.isdir('F:\System_ISO'))
-print(os.path.join('E:/Blog', '/c/d'))
+print(os.path.join('E:/Blog' '/moban'))
+# 文件拼接
+print("--------------")
 
+# pathlib — Object-oriented filesystem paths
 from pathlib import Path
 
 p = Path('.')
 print(p.resolve())
+# 当前的绝对路径
+print("--------------")
 print(p.is_dir())
-# 创建目录
 # q = Path('E:/a/b/c')
 # Path.mkdir(q, parents=True)
+# 创建目录
